823e6e0f9eb60a89c1381d12507b7df9cebc2ec2
VSX13 <61202368+VSX13@users.noreply.github.com>
2020-05-06 02:41:43 +0700

    Add files via upload

---
+++ grafik_doughnuttotaldeaths.php
@@ -0,0 +1,85 @@
+<?php
+$koneksi    = mysqli_connect("localhost", "root", "", "db_country");
+$nama_country  = mysqli_query($koneksi, "SELECT country FROM tb_country");
+$total_deaths   = mysqli_query($koneksi, "SELECT total FROM tb_tdeaths");
+?>
+
+<!doctype html>
+<html>
+ 
+<head>
+	<title>Bar Doughnut Total Deaths</title>
+	<script type="text/javascript" src="Chart.js"></script>
+</head>
+ 
+<body>
+	<div id="canvas-holder" style="width:50%">
+		<canvas id="chart-area"></canvas>
+	</div>
+	<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Bar Doughnut Total Deaths</title>
+    <script src="js/Chart.js"></script>
+    <style type="text/css">
+            .container {
+                width: 40%;
+                margin: 15px auto;
+            }
+    </style>
+  </head>
+  <body>
+
+    <div class="container">
+        <canvas id="doughnutchart" width="100" height="100"></canvas>
+    </div>
+
+  </body>
+</html>
+
+<script  type="text/javascript">
+  var ctx = document.getElementById("doughnutchart").getContext("2d");
+  var data = {
+            labels: [<?php while ($p = mysqli_fetch_array($nama_country)) { echo '"' . $p['country'] . '",';}?>],
+            datasets: [
+            {
+              label: "Total Deaths",
+              data: [<?php while ($p = mysqli_fetch_array($total_deaths)) { echo '"' . $p['total'] . '",';}?>],
+              backgroundColor: [
+              'rgb(255, 0, 0)',
+			  'rgb(255, 255, 0)',
+			  'rgb(34, 139, 35)',
+			  'rgb(75, 0, 130)',
+			  'rgb(62, 254, 255)',
+			  'rgb(127, 255, 1)',
+			  'rgb(238, 130, 238)',
+			  'rgb(210, 180, 140)',
+			  'rgb(112, 128, 145)',
+			  'rgb(25, 25, 112)'
+              ],
+					borderColor: [
+					'rgb(255, 0, 0)',
+					'rgb(255, 255, 0)',
+					'rgb(34, 139, 35)',
+					'rgb(75, 0, 130)',
+					'rgb(62, 254, 255)',
+					'rgb(127, 255, 1)',
+					'rgb(238, 130, 238)',
+					'rgb(210, 180, 140)',
+					'rgb(112, 128, 145)',
+					'rgb(25, 25, 112)'
+					]
+            }
+            ]
+            };
+
+  var mydoughnutchart = new Chart(ctx, {
+                  type: 'doughnut',
+                  data: data,
+                  options: {
+                    responsive: true
+                }
+              });
+
+</script>
---
+++ grafik_totaldeaths.php
@@ -0,0 +1,50 @@
+<?php
+include('koneksi2.php');
+$country = mysqli_query($koneksi,"select * from tb_country");
+while($row = mysqli_fetch_array($country)){
+	$nama_country[] = $row['country'];
+	
+	$query = mysqli_query($koneksi,"select sum(total) as total from tb_tdeaths where id_country='".$row['id_country']."'");
+	$row = $query->fetch_array();
+	$total_deaths[] = $row['total'];
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bar Chart Total Deaths</title>
+	<script type="text/javascript" src="Chart.js"></script>
+</head>
+<body>
+	<div style="width: 800px;height: 800px">
+		<canvas id="myChart"></canvas>
+	</div>
+ 
+ 
+	<script>
+		var ctx = document.getElementById("myChart").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: <?php echo json_encode($nama_country); ?>,
+				datasets: [{
+					label: 'Total Deaths',
+					data: <?php echo json_encode($total_deaths); ?>,
+					backgroundColor: 'rgba(255, 0, 0, 1)',
+					borderColor: 'rgba(255, 0, 0, 1)',
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+	</script>
+</body>
+</html>
